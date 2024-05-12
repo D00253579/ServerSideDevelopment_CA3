f5a534070f472a5cf0d85531d374bcfb23dcf8eb
caitlinmag <caitlinmaguire14@outlook.com>
2024-05-12 17:02:50 +0100

    Completed the create character functionality.
    Also added some styling to the create character page.

--- app/Http/Controllers/CharacterController.php
+++ app/Http/Controllers/CharacterController.php
@@ -18,10 +18,68 @@ class characterController extends Controller
         return view('characters.create');
     }
 
+    /**
+     *   Store the new disney characters 
+    */
+    public function store(Request $request){
+    $request->validate([
+        'characterName' => 'required',
+        'characterType' => 'required',
+        'movieName' => 'required',
+        'characterQuote' => 'required',
+        'image_path' => 'required|mimes:jpg,png,jpeg|max:5048'
+    ]);
+
+    $newCharacterImage = uniqid() . '-' . $request->characterName . '.' . $request->image_path->extension();
+
+    $request->image_path->move(public_path('Images'), $newCharacterImage);
+
+    /**
+     *  To fix issue with the foreign key:
+     *  we are checking what studio the character is from based on the user drop down list,
+     *  setting the selectedStudioId equal to the corresponding ID from 1-5
+     */
+
+    $userOption = $request->input('studio');
+    $selectedStudioId; 
+    if($userOption == "waltDisney"){
+        $selectedStudioId = 1;
+    }
+
+    if($userOption == "pixar"){
+        $selectedStudioId = 2;
+    }
+
+    if($userOption == "marvel"){
+        $selectedStudioId = 3;
+    }
+
+    if($userOption == "20thCentury"){
+        $selectedStudioId = 4;
+    }
+
+    if($userOption == "lucasFilms"){
+        $selectedStudioId = 5;
+    }
+
+    disneyCharacters::create([
+    'characterName' => $request->input('characterName'),
+    'characterType' => $request->input('characterType'),
+    'movieName' => $request->input('movieName'),
+    'characterQuote' => $request->input('characterQuote'),
+    'studio_id' => $selectedStudioId,
+    'image_path'=> $newCharacterImage
+    ]);
+
+    return redirect(('/characters'))
+            ->with('message', 'Yay your character has been created!');
+    }
+
     public function edit($characterName){
         return view('characters.edit')
         ->with ('character',disneyCharacters::where('characterName',$characterName)->first());
     }
+
     public function update(Request $request, $characterName)
     {
         $request->validate([
--- resources/views/characters/create.blade.php
+++ resources/views/characters/create.blade.php
@@ -1,22 +1,61 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="w-4/5 m-auto text-left">
-    <div class="py-15">
+<div class="w-4/5 m-auto">
+    <div class="mt-16 flex justify-center">
         <h1 class="text-6xl">
-            <img src="Images/addTitle.png" alt="Update Title Image" height="50" width="80">
+            <img src="/Images/addTitle.png" alt="Update Title Image" height="300" width="250" class="flex justify-center">
         </h1>
     </div>
 </div>
-<form action="/characters/{{ $character->characterName }}" method="POST" enctype="multipart/form-data">
+<div class=" flex justify-center">
+<form action="/characters" method="POST" enctype="multipart/form-data">
     @csrf
-    @method('PUT')
-
-    <textarea class='CreateFields' name='characterName'>{{ $character->characterName }}</textarea>
-    <textarea class='CreateFields' name='characterType'>{{ $character->characterType }}</textarea>
-    <textarea class='CreateFields' name="movieName" >{{ $character->movieName }}</textarea>
-    <textarea class='CreateFields' name="characterQuote" >{{ $character->characterQuote }}</textarea>
-    <button type="submit">Add</button>
-
+    <div class="inline-block">
+        <br>
+    <h2 class="createLabels">Character Name:</h2>
+    <br>
+<input type="text" name="characterName" placeholder="Enter Character Name" class="createField">
+<br>
+<br>
+<h2 class="createLabels">Character Type: </h2>
+<br>
+<input type="text" name="characterType" placeholder="Enter Character Type" class="createField">
+<br>
+<br>
+<h2 class="createLabels">Movie Name: </h2>
+<br>
+<input type="text" name="movieName" placeholder="Enter Movie Name" class="createField">
+<br>
+<br>
+<h2 class="createLabels">Character Quote: </h2>
+<br>
+<input type="text" name="characterQuote" placeholder="Enter Character Quote" class="createField">
+<br>
+<br>
+<h2 class="createLabels mt-8">Upload a Character Image:</h2>
+<br>
+<input type="file" name="image_path" class="addImageField">
+<br>
+<br>
+<div class="flex justify-center inline-block mt-4">
+<label for="selectStudio" class="text-darkBlue font-serif">Select a Studio:</label>
+<br>
+<br>
+<select name="studio" class="mt-4" id="studio">
+<option value="waltDisney">Walt Disney</option>
+<option value="pixar">Pixar</option>
+<option value="marvel">Marvel</option>
+<option value="20thCentury">20th Century</option>
+<option value="lucasFilms">Lucasfilms</option>
+</select>
+<br>
+<br>
+</div>
+<br>
+<div class="flex justify-center mb-20">
+<button type="submit" class="addButton">Submit</button>
+</div>
+    </div>
 </form>
 @endsection 
